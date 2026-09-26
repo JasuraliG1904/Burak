13365860317ffd71c6f9f03d16ce2615d5ad4e71
Jasurbek1904 <jasuraliqosimjonov@gmail.com>
2026-09-26 16:40:00 +0900

    53-54-n task

--- src/train.ts
+++ src/train.ts
@@ -1,11 +1,22 @@
-const getSquareNumbers = (arr: number[]): { number: number; square: number }[] => {
-    return arr.map(num => ({
-        number: num,
-        square: num ** 2
-    }));
-};
+// const getSquareNumbers = (arr: number[]): { number: number; square: number }[] => {
+//     return arr.map(num => ({
+//         number: num,
+//         square: num ** 2
+//     }));
+// };
 
 
-const numbers: number[] = [1, 2, 3];
-const result = getSquareNumbers(numbers);
-console.log(result);
+// const numbers: number[] = [1, 2, 3];
+// const result = getSquareNumbers(numbers);
+// console.log(result);
+
+
+// N task
+function palindromCheck(str: string): boolean {
+    const reversed: string = str.split('').reverse().join('');
+    return str === reversed;
+}
+
+console.log(palindromCheck("dad")); // true
+console.log(palindromCheck("son")); // false
+console.log(palindromCheck("aziza")); // true
